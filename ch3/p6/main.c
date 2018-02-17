f648104b01ecf09de2d79448ac4c8901cfe0e09a
Greg Donald <gdonald@gmail.com>
2018-02-16 20:48:42 -0600

    fix typo

--- ch3/p6/main.c
+++ ch3/p6/main.c
@@ -4,7 +4,7 @@ int main(void)
 {
   int n1, n2, d1, d2, rn, rd;
 
-  printf("Enter two fractions seperated by a plus sign: ");
+  printf("Enter two fractions separated by a plus sign: ");
   scanf("%d/%d+%d/%d", &n1, &d1, &n2, &d2);
 
   rn = n1 * d2 + n2 * d1;
