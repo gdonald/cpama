eed1231e2018230476efea531e746d6eef379ba4
Greg Donald <gdonald@gmail.com>
2018-02-16 20:47:31 -0600

    ch3 p6

---
+++ ch3/p6/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n1, n2, d1, d2, rn, rd;
+
+  printf("Enter two fractions seperated by a plus sign: ");
+  scanf("%d/%d+%d/%d", &n1, &d1, &n2, &d2);
+
+  rn = n1 * d2 + n2 * d1;
+  rd = d1 * d2;
+
+  printf("The sum is %d/%d\n", rn, rd);
+  
+  return 0;
+}
