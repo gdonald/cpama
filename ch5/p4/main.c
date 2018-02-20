1d38180d42ded8a2797d038bb7425bd7106df4e0
Greg Donald <gdonald@gmail.com>
2018-02-20 07:14:46 -0600

    ch5 p4

---
+++ ch5/p4/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int speed;
+
+  printf("Enter wind speed in knots: ");
+  scanf("%d", &speed);
+
+  printf("The wind description is: ");
+  
+  if(speed < 1)
+  {
+    printf("Calm");
+  }
+  else if(speed < 3)
+  {
+    printf("Light air");
+  }
+  else if(speed < 27)
+  {
+    printf("Breeze");
+  }
+  else if(speed < 47)
+  {
+    printf("Gale");
+  }
+  else if(speed < 63)
+  {
+    printf("Storm");
+  } 
+  else
+  {
+    printf("Hurricane");
+  }
+
+  printf("\n");
+
+  return 0;
+}
