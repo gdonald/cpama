3f0397dd91e4f0e70a5e303e3a35ce916fed0e68
Greg Donald <gdonald@gmail.com>
2018-02-25 19:19:18 -0600

    ch04 p04

--- ch03/p04/main.c
+++ ch03/p04/main.c
@@ -5,13 +5,9 @@ int main(void)
   int area, prefix, number;
 
   printf("Enter phone number: [(xxx) xxx-xxxx]: ");
-  scanf("%d-%d-%d-%d-%d", &area, &prefix, &number);
+  scanf("(%3d) %3d-%4d", &area, &prefix, &number);
 
-  printf("GS1 prefix: %d\n", gs1);
-  printf("Group identifier: %d\n", group);
-  printf("Publisher code: %d\n", publisher);
-  printf("Item number: %d\n", item);
-  printf("Check digit: %d\n", check);
+  printf("You entered: %d.%d.%d\n", area, prefix, number);
   
   return 0;
 }
