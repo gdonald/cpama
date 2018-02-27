8d73ac8eddf718b15aa5eeabbd4db057ab4819e3
Greg Donald <gdonald@gmail.com>
2018-02-26 19:38:26 -0600

    ch07 p11

---
+++ ch07/p11/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char c, first;
+  
+  printf("Enter a first and last name: ");
+
+  while((c = getchar()) == ' ');
+  first = c;
+
+  while((c = getchar()) != ' ');
+
+  while((c = getchar()) != '\n')
+  {
+    if(c != '\n')
+    {
+      printf("%c", c);
+    }
+  }
+
+  printf(", %c.\n", first);
+
+  return 0;
+}
