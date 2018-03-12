f190422739e3171ddc49c6f661db748200663c33
Greg Donald <gdonald@gmail.com>
2018-03-12 17:44:00 -0500

    ch12 p04

---
+++ ch12/p04/main.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 1024
+
+int main(void)
+{
+  char a[N], c;
+  char *p = a, *q;
+  
+  printf("Enter a message: ");
+
+  while((c = getchar()) != '\n' && a < a + N)
+  {
+    c = tolower(c);
+
+    if(c >= 'a' && c <= 'z')
+    {
+      *p++ = c;
+    }
+  }
+
+  q = a;
+
+  while(p-- > a)
+  {
+    if(*p != *q++)
+    {
+      printf("Not a palindrome\n");
+      exit(EXIT_SUCCESS);
+    }
+  }
+
+  printf("Palindrome\n");
+
+  return 0;
+}
