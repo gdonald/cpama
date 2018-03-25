5f41aeb67288ab3a6451fba3756065ca242b4ee4
Greg Donald <gdonald@gmail.com>
2018-03-25 18:41:44 -0500

    ch14 e04a

--- ch14/e04a/main.c
+++ ch14/e04a/main.c
@@ -5,8 +5,8 @@
 
 int main(void)
 {
-  printf("3 divided by the average of 4 and 5 is %.2g\n",   (float) 3 / AVG(4,5));
-  printf("3 divided by the average of 4 and 5 is %.2g\n", (float) 3 / FIXED(4,5));
+  printf("3 divided by the average of 4 and 5 is %.g\n",   (float) 3 / AVG(4,5));
+  printf("3 divided by the average of 4 and 5 is %.g\n", (float) 3 / FIXED(4,5));
   
   return 0;
 }
