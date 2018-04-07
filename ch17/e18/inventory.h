6c77d8753dc3900c8fb4883fed0584a1f3bf4cb5
Greg Donald <gdonald@gmail.com>
2018-04-06 19:22:31 -0500

    ch17 e18

---
+++ ch17/e18/inventory.h
@@ -0,0 +1,23 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#define NAME_LEN 25
+#define MAX_PARTS 100
+
+struct part
+{
+  int number;
+  char name[NAME_LEN + 1];
+  int on_hand;
+  float price;
+};
+
+int compare_parts(const void *p, const void *q);
+int find_part(int number, struct part *inventory, int *num_parts);
+void insert(struct part *inventory, int *num_parts);
+void search(struct part *inventory, int *num_parts);
+void update(struct part *inventory, int *num_parts);
+void update_price(struct part *inventory, int *num_parts);
+void print(struct part *inventory, int *num_parts);
+
+#endif
