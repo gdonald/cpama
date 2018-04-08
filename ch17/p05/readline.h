c68b54e95ebc50dcbd852c41483df4d97dfe450d
Greg Donald <gdonald@gmail.com>
2018-04-08 18:54:04 -0500

    ch17 p05

---
+++ ch17/p05/readline.h
@@ -0,0 +1,14 @@
+#ifndef READLINE_H
+#define READLINE_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 20
+
+int read_line(char str[], int n);
+
+#endif
+
