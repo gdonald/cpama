42668f3a7f500c48ab23f34a49f65d5014ca276d
Greg Donald <gdonald@gmail.com>
2018-05-12 07:24:59 -0500

    ch20 e11

---
+++ ch20/e11/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define SHIFT_BIT 1
+#define CTRL_BIT  2
+#define ALT_BIT   4
+
+int main(void)
+{
+  int key_code = 8;
+
+  if((key_code & (SHIFT_BIT | CTRL_BIT | ALT_BIT)) == 0)
+  {
+    printf("No modifier keys pressed\n");
+  }
+  
+  return 0;
+}
